c113c4e21b18830714b171eb1fc4f58cfa257088
LukeBrazil <Luke.Brazil13@icloud.com>
2021-02-20 13:20:11 -0600

    scrape basic html page for information

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import requests
+from bs4 import BeautifulSoup
 
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+    soup = BeautifulSoup(content, 'lxml')
+    course_cards = soup.find_all('div', class_='card')
+    for card in course_cards:
+        course_name = card.h5.text
+        course_price = card.a.text.split()[-1]
+        print(f"Course Name: {course_name} -- Course Price: {course_price}")
